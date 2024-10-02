c799eaccd965148e81b9ca0e344dd1c419b0552c
alexandrawilkowski <alexandrawilkowski@localhost>
2024-10-02 15:29:30 -0500

    Studio4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,31 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent= in.nextInt();
+		int blueComponent= in.nextInt();
+		boolean isFilled= in.nextBoolean();
+		double parameterOne= in.nextDouble();
+		double parameterTwo= in.nextDouble();
+		double parameterThree= in.nextDouble();
+		double parameterFour= in.nextDouble();
+
+	
+		
+		System.out.print(parameterThree);
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		StdDraw.rectangle(parameterOne,parameterTwo,parameterThree,parameterFour);
+		
+		
+		
+//		
+//		double x= in.nextDouble();
+//		double y= in.nextDouble();
+//		double halfWidth= in.nextDouble();
+//		double halfHeight= in.nextDouble(); 
+		
+//		StdDraw.rectangle (x, y, halfWidth, halfHeight);
+		
 	}
 }
